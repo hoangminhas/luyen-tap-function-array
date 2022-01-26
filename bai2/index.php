52a90c99d326c648c1b99f65b7f8ce9ac3462e00
hoangminhas <hoangminh9973@gmail.com>
2022-01-26 11:41:27 +0700

    continuing

--- bai2/index.php
+++ bai2/index.php
@@ -9,24 +9,66 @@
 </head>
 <body>
 <h1>Menu</h1>
-<div>
-    <ol>
-        <li><a href="">Nhap mang</a></li>
-        <li><a href="">Tim so lon nhat trong mang</a></li>
-        <li><a href="">Tims so nho nhat trong mang</a></li>
-        <li><a href="">Tim so duong chan lon nhat trong mang</a></li>
-        <li><a href="">Tim so am le nho nhat trong mang</a></li>
-        <li><a href="">Tim cac so chinh phuong trong mang</a></li>
-        <li><a href="">Tinh tong mang</a></li>
-        <li><a href="">Tinh trung binh cong cac phan tu mang</a></li>
-        <li><a href="">Tim cac phan tu lon hon trung binh cong</a></li>
-        <li><a href="">Sap xep mang theo trat tu tang dan</a></li>
-        <li><a href="">Sap xep mang theo trat tu giam dan</a></li>
-    </ol>
-</div>
-<form action="#" method="post">
-    <input type="text" name="input" size="60" placeholder="nhap mang dang: [x,y,z,...]"><br>
-    <button>Create</button>
+<form id="form" action="#" method="post">
+    <input type="text" name="input" size="60" placeholder="nhap mang theo dang: x y z..."><br>
+    <select name="select" id="select">
+        <option value="max">Tim so lon nhat</option>
+        <option value="min">Tim so nho nhat</option>
+        <option value="max-positive-even">Tim so duong chan lon nhat</option>
+        <option value="min-negative-odd">Tim so am le nho nhat</option>
+        <option value="square-num">Tim cac so chinh phuong</option>
+        <option value="sum">Tinh tong mang</option>
+        <option value="sum-average">Tinh trung binh cong</option>
+        <option value="above-average">Tim cac phan tu lon hon TBC</option>
+        <option value="sort-asc">Sap xep tang dan</option>
+        <option value="sort-des">Sap xep giam dan</option>
+    </select>
+    <button>Let's go</button>
 </form>
 </body>
 </html>
+<?php
+if ($_SERVER["REQUEST_METHOD"] == "POST") {
+    $str = $_REQUEST['input'];
+    $arr = explode(' ', $str);
+    $select = $_REQUEST['select'];
+
+    //find max
+    if ($select == "max") {
+        echo findMaxInArr($arr);
+    }
+
+    //find min
+    if ($select == "min") {
+        echo findMinInArr($arr);
+    }
+
+    //find max positive even
+
+}
+
+function findMaxInArr($arr)
+{
+    $max = $arr[0];
+    foreach ($arr as $value) {
+        if ($max < $value) {
+            $max = $value;
+        }
+    }
+    return $max;
+}
+function findMinInArr($arr)
+{
+    $min = $arr[0];
+    foreach ($arr as $value) {
+        if ($min > $value) {
+            $min = $value;
+        }
+    }
+    return $min;
+}
+function findMaxPositiveEven($arr)
+{
+
+}
+?>
