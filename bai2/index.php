e44044c0104d8438fd30cd842d39819b136fcbc1
hoangminhas <hoangminh9973@gmail.com>
2022-01-26 11:03:19 +0700

    update bai 2

---
+++ bai2/index.php
@@ -0,0 +1,32 @@
+<!doctype html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport"
+          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Menu function</title>
+</head>
+<body>
+<h1>Menu</h1>
+<div>
+    <ol>
+        <li><a href="">Nhap mang</a></li>
+        <li><a href="">Tim so lon nhat trong mang</a></li>
+        <li><a href="">Tims so nho nhat trong mang</a></li>
+        <li><a href="">Tim so duong chan lon nhat trong mang</a></li>
+        <li><a href="">Tim so am le nho nhat trong mang</a></li>
+        <li><a href="">Tim cac so chinh phuong trong mang</a></li>
+        <li><a href="">Tinh tong mang</a></li>
+        <li><a href="">Tinh trung binh cong cac phan tu mang</a></li>
+        <li><a href="">Tim cac phan tu lon hon trung binh cong</a></li>
+        <li><a href="">Sap xep mang theo trat tu tang dan</a></li>
+        <li><a href="">Sap xep mang theo trat tu giam dan</a></li>
+    </ol>
+</div>
+<form action="#" method="post">
+    <input type="text" name="input" size="60" placeholder="nhap mang dang: [x,y,z,...]"><br>
+    <button>Create</button>
+</form>
+</body>
+</html>
